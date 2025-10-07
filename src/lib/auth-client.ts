64f9bade23b1c6c480b5b97f8d299b892cfcfeda
PeraSite <pretocki3@gmail.com>
2025-10-07 19:40:11 +0900

    style: Prettier 적용

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,12 +3,12 @@ import { expoClient } from "@better-auth/expo/client";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-    baseURL: "http://127.0.0.1:3000", // Base URL of your Better Auth backend.
-    plugins: [
-        expoClient({
-            scheme: "didim",
-            storagePrefix: "didim",
-            storage: SecureStore,
-        })
-    ]
+  baseURL: "http://127.0.0.1:3000", // Base URL of your Better Auth backend.
+  plugins: [
+    expoClient({
+      scheme: "didim",
+      storagePrefix: "didim",
+      storage: SecureStore,
+    }),
+  ],
 });
