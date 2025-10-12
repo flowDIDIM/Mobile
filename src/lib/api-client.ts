a9cc73e290a0d8efce5a16d7991120f8cce8c7a7
PeraSite <pretocki3@gmail.com>
2025-10-12 19:20:08 +0900

    feat: Hono API Client 사용하게 수정

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,37 +1,6 @@
-import axios from "axios";
-import { authClient } from "./auth-client";
 import { env } from "@/env";
 
-const apiClient = axios.create({
-  baseURL: env.EXPO_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+import type { AppType } from "../../../DIDIM-Server/src/app";
+import { hc } from "hono/client";
 
-apiClient.interceptors.request.use(
-  async (config) => {
-    const session = await authClient.getSession();
-
-    if (session?.data?.session?.token) {
-      config.headers.Authorization = `Bearer ${session.data.session.token}`;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-
-apiClient.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response?.status === 401) {
-      await authClient.signOut();
-    }
-    return Promise.reject(error);
-  },
-);
-
-export { apiClient };
+export const client = hc<AppType>(env.EXPO_PUBLIC_API_URL);
