7bcdd97be180bc62be3e82aba14f7adaf685e774
PeraSite <pretocki3@gmail.com>
2025-10-13 14:27:03 +0900

    fix: Hono Clien에 Authenication 추가

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -3,6 +3,15 @@ import { env } from "@/env";
 import type { AppType } from "../../../DIDIM-Server/src/app";
 import { hc } from "hono/client";
 import { hcQuery } from "@/lib/hono-rpc";
+import { authClient } from "@/lib/auth-client";
 
-export const client = hc<AppType>(env.EXPO_PUBLIC_API_URL);
-export const clientQuery = hcQuery(hc<AppType>(env.EXPO_PUBLIC_API_URL));
+export const client = hc<AppType>(env.EXPO_PUBLIC_API_URL, {
+  headers: () => {
+    const cookies = authClient.getCookie();
+
+    return {
+      Cookie: cookies,
+    };
+  },
+});
+export const clientQuery = hcQuery(client);
