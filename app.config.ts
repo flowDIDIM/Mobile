183a813c8893d892b0b7b0008b740ac607ef381e
PeraSite <pretocki3@gmail.com>
2025-10-13 14:26:50 +0900

    chore: 안드로이드 패키지명 추가

--- app.config.ts
+++ app.config.ts
@@ -14,6 +14,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     supportsTablet: true,
   },
   android: {
+    package: "com.didim.android",
     adaptiveIcon: {
       backgroundColor: "#E6F4FE",
       foregroundImage: "./assets/images/android-icon-foreground.png",
