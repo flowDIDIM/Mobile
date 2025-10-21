55898245abc7625eea3b45af5ef382e64678f471
PeraSite <pretocki3@gmail.com>
2025-10-22 01:36:08 +0900

    fix: 번들링 타입

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,9 +1,10 @@
 import { env } from "@/env";
 
 import { authClient } from "@/lib/auth-client";
-import { hcWithType } from "../../../DIDIM-Server/dist/app";
+import { type AppType } from "../../../DIDIM-Server/dist/app";
+import { hc } from "hono/client";
 
-export const client = hcWithType(env.EXPO_PUBLIC_API_URL, {
+export const client = hc<AppType>(env.EXPO_PUBLIC_API_URL, {
   headers: () => {
     const cookies = authClient.getCookie();
 
